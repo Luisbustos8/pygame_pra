f281190f14a176a1dcace68778ec05b778ca027e
Luisillo Programación <Luisillo@MacBook-Pro-de-LUIS.local>
2020-04-18 17:45:16 +0200

    campo de juegos creado

---
+++ turtlerace.py
@@ -0,0 +1,30 @@
+import pygame, sys
+
+class Game():
+    Runners = []
+    __starline = 20
+    __finishline = 620
+    
+
+    def __init__(self):
+        self.__screen = pygame.display.set_mode((640, 480))
+        self.__background = pygame.image.load("fondo/playa.png")
+        pygame.display.set_caption("Carrera de caballitos")
+     
+    def competir(self):
+        gameOver = False
+        while not gameOver:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    gameOver = True
+            
+        self.__screen.blit(self._background, (0, 0))
+        
+        pygame.display.flip()
+    
+    
+    
+if__name__="__main__":
+    game = Game()
+    pygame.font.init()
+    game.competir()
